5f16fe68d8bc8b2437a50be0651d442a50cd8563
Davis Weimer <davisweimer@gmail.com>
2023-05-12 07:09:16 -0600

    Create LinkedList RSpec

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,18 @@
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe LinkedList do
+  it 'exists' do
+    list = LinkedList.new
+
+    expect(list).to be_instance_of(LinkedList)
+  end
+
+  it 'has a head' do
+    list = LinkedList.new
+
+    expect(list.head).to eq(nil)
+  end
+
+  
+end
