5cf9982b113084d02af31ccf3a81465488f9bb0b
Davis Weimer <davisweimer@gmail.com>
2023-05-13 15:45:14 -0600

    Create test for multiple appends/counts

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe LinkedList do
     expect(list.head.next_node).to eq nil
   end
 
-  it 'counts one node in link' do
+  it 'counts one node in linked list' do
     list = LinkedList.new
     list.append("doop")
 
@@ -34,5 +34,34 @@ RSpec.describe LinkedList do
     list.append("doop")
 
     expect(list.to_string).to eq("doop")
+  end
+
+  it 'can append more strings' do
+    list = LinkedList.new
+    list.append("doop")
+    
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node).to eq nil
+    
+    list.append("deep")
+    
+    expect(list.head.next_node.data).to eq("deep")
+    expect(list.head.next_node.next_node).to eq nil
+  end
+
+  it 'can count multiple nodes in linked list' do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+
+    expect(list.count).to eq 2
+  end
+
+  it 'can multiple node data to string' do
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+
+    expect(list.to_string).to eq("doop deep")
   end
 end
