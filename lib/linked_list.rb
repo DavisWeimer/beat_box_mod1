795536e0c31211a3e926d0266f4203d0f76b7752
Davis Weimer <davisweimer@gmail.com>
2023-05-13 15:43:57 -0600

    Update LinkedList Class: append, count, to_string

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,22 +2,32 @@ class LinkedList
   attr_reader :head
   def initialize
     @head = head
-    @node_counter = 0
+    @node_counter = 1
   end
 
-  def append(music)
+  def append(str)
     if @head == nil
-      @head = Node.new(music)
-      @node_counter += 1
+      @head = Node.new(str)
+    else
+      @next_node = Node.new(str)
+      @head.next_node = @next_node
     end
   end
-
+  
   def count
-    @node_counter
+    if @next_node != nil
+      @node_counter += 1
+    elsif @next_node == nil
+      @node_counter
+    end
   end
-
+  
   def to_string
-    @head.data
+    if count == 1 
+      @head.data
+    else
+      # "#{@head.data} #{@next_node.data}"
+      @head.data + " " + @next_node.data
+    end
   end
-
 end
